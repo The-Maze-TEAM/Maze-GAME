cd060b743c26a37bbe32b464c8befa873eda52f3
AnActualBanana <loganmcclain59@gmail.com>
2023-02-22 14:09:08 -0600

    gamemode dont destroy on load

--- Assets/Scripts/GameModes/GameModeController.cs
+++ Assets/Scripts/GameModes/GameModeController.cs
@@ -8,7 +8,14 @@ public class GameModeController : MonoBehaviour
 {
     public GameObject[] ModeControllers;
 
+    void Awake() { // stops controller from being destroyed on scene load.
+    //DO NOT TOUCH
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
     public void FindGameMode(string mode) // finds gamemode passed as string, enables the controller for that mode
+    //Use OnEnable() in your gamemode as an initialize/setup function
     {
         foreach(GameObject modeController in ModeControllers)
         {
