13322dfda37ee8dab8116d265110c612560b534f
magicaldave <smokydave@icloud.com>
2023-02-26 21:05:01 -0600

    Edit: added listener script to create Network events from the ui

--- Assets/Scripts/NetManUI.cs
+++ Assets/Scripts/NetManUI.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Netcode;
 
 public class NetManUI : MonoBehaviour
 {
 	[SerializeField] private Button serverBtn;
-	[SerializeField] private Button clientBtn;
 	[SerializeField] private Button hostBtn;
+	[SerializeField] private Button clientBtn;
 
-	void start()
+	private void Awake()
 	{
-
-	}
-
-	void update()
-	{
-
+		serverBtn.onClick.AddListener(() =>
+		{
+			NetworkManager.Singleton.StartServer();
+		});
+		hostBtn.onClick.AddListener(() =>
+		{
+			NetworkManager.Singleton.StartHost();
+		});
+		clientBtn.onClick.AddListener(() =>
+		{
+			NetworkManager.Singleton.StartClient();
+		});
 	}
 }
