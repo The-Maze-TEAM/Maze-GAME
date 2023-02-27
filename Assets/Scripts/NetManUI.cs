e93593de01cff2d7b33dce5ec1827691435aada3
magicaldave <smokydave@icloud.com>
2023-02-26 19:08:28 -0600

    Add: Scripts which control the network manager UI. For debug purposes only, we'll remove these at some point.

---
+++ Assets/Scripts/NetManUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetManUI : MonoBehaviour
+{
+	[SerializeField] private Button serverBtn;
+	[SerializeField] private Button clientBtn;
+	[SerializeField] private Button hostBtn;
+
+	void start()
+	{
+
+	}
+
+	void update()
+	{
+
+	}
+}
